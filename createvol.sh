ef6534ce3ac22496175e6e26665534e4e44101df
mvaskuri <86118939+mvaskuri@users.noreply.github.com>
2023-02-06 15:24:01 +0530

    Create createvol.sh

---
+++ createvol.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+aws --version
+i=1
+for vol in {$i -le 3}
+do
+aws ec2 create-volume \
+--volume-type gp2 \
+--size 1 \
+--availability-zone us-east-1a
+echo "created Volume $i"
+sleep 2
+done
+
+Script to delete volume with 1GB.
